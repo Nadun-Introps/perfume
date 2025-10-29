fa1de83785b6a36e2bdb1bdbd7d6196d0bc5a3fb
Chanuka Wanigasekara <chanukawanigasekara007@gmail.com>
2025-10-29 11:49:17 +0530

    footer update
    footer and product card update

--- packages/Webkul/Shop/src/Resources/views/components/layouts/footer/index.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/footer/index.blade.php
@@ -11,178 +11,159 @@
     template as much as possible.
 -->
 @php
-  $channel = core()->getCurrentChannel();
+    $channel = core()->getCurrentChannel();
 
-  $customization = $themeCustomizationRepository->findOneWhere([
-    'type' => 'footer_links',
-    'status' => 1,
-    'theme_code' => $channel->theme,
-    'channel_id' => $channel->id,
-  ]);
+    $customization = $themeCustomizationRepository->findOneWhere([
+        'type' => 'footer_links',
+        'status' => 1,
+        'theme_code' => $channel->theme,
+        'channel_id' => $channel->id,
+    ]);
 @endphp
 
-<footer class="store-footer basic-footer" style="background-color: #f2f2f2 !important; color: black !important;">
-<div aria-label="footer" class="store-footer__inner">
-    <div class="container">
-      <div class="grid grid-cols-2 lg:grid-cols-6 lg:gap-12"> <!-- Changed gap-6 to gap-12 -->
-        <div class="mb-2.5 col-span-2 ">
+<footer class="mp-footer">
+    <div class="mp-footer__main">
+        <div class="mp-container">
+            <div class="mp-footer__columns">
+                <!-- First Column: Logo, VAT Info, Payment Methods -->
+                <div class="mp-footer__column mp-footer__column--left">
+                    <a href="{{ route('shop.home.index') }}" class="mp-footer__logo" aria-label="logo">
+                        <h1 class="mp-footer__logo-text">SwissDuft</h1>
+                    </a>
+                    <div class="mp-footer__vat-info">
+                        <p>VAT Account Number : 104786666800003</p>
+                    </div>
+                    <div class="mp-footer__payment-methods">
+                        <ul class="mp-payment-list">
+                            <li class="mp-payment-item">
+                                <img src="{{ asset('images/mada_mini.png') }}" alt="mada" width="40"
+                                    height="20" class="mp-payment-img">
+                            </li>
+                            <li class="mp-payment-item">
+                                <img src="{{ asset('images/credit_card_mini.png') }}" alt="credit_card" width="40"
+                                    height="20" class="mp-payment-img">
+                            </li>
+                            <li class="mp-payment-item">
+                                <img src="{{ asset('images/bank_mini.png') }}" alt="bank" width="40"
+                                    height="20" class="mp-payment-img">
+                            </li>
+                            <li class="mp-payment-item">
+                                <img src="{{ asset('images/apple_pay_mini.png') }}" alt="apple_pay" width="40"
+                                    height="20" class="mp-payment-img">
+                            </li>
+                            <li class="mp-payment-item">
+                                <img src="{{ asset('images/tamara_installment_mini.png') }}" alt="tamara_installment"
+                                    width="25" height="20" class="mp-payment-img">
+                            </li>
+                            <li class="mp-payment-item">
+                                <img src="{{ asset('images/cod_mini.png') }}" alt="cod" width="40"
+                                    height="20" class="mp-payment-img">
+                            </li>
+                        </ul>
+                    </div>
+                </div>
 
-          <a href="https://uae.matchperfumes.com/en/" class="footer-logo " aria-label="logo">
-            <!-- <img src="./Best Sellers - Match Perfumes UAE_files/5XQkVJR3f2Iu9N4DvNhWcGvRkrPqirua0NVxyT2B.png"
-              height="&quot;64&quot;" style=" height:64px;" alt="Perfume" class="img-fluid mx-auto lg:mx-[unset]"> -->
-              <h1 class="mb-2.5">Perfume</h1>
-          </a>
-          <div class="flex items-left justify-left md:justify-start rtl:lg:ml-2 ltr:lg:mr-2 lg:mb-0" style="padding-top: 35px; padding-bottom: 15px;">
-            <p class="text-sm">VAT Account Number
-              :
-              104786666800003</p>
-          </div>
-                  <div>
-          <ul class="payment-methods">
-            <li class="item-method">
-              <img src="{{ asset('images/mada_mini.png') }}" alt="mada" width="50" height="30" class="payment-img">
-            </li>
-            <li class="item-method">
-              <img src="{{ asset('images/credit_card_mini.png') }}" alt="credit_card" width="50" height="30"
-                class="payment-img">
-            </li>
-            <li class="item-method">
-              <img src="{{ asset('images/bank_mini.png') }}" alt="bank" width="50" height="30" class="payment-img">
-            </li>
-            <li class="item-method">
-              <img src="{{ asset('images/apple_pay_mini.png') }}" alt="apple_pay" width="50" height="30"
-                class="payment-img">
-            </li>
-            <li class="item-method">
-              <img src="{{ asset('images/tamara_installment_mini.png') }}" alt="tamara_installment"
-                style="width:35px; height:30px;" class="payment-img">
-            </li>
-            <li class="item-method">
-              <img src="{{ asset('images/cod_mini.png') }}" alt="cod" width="50" height="30" class="payment-img">
-            </li>
-          </ul>
+                <!-- Second Column: Important Links -->
+                <div class="mp-footer__column mp-footer__column--middle">
+                    <h3 class="mp-footer__title">Important Links</h3>
+                    <ul class="mp-footer__links">
+                        <li class="mp-footer__link-item">
+                            <a href="/page/about-us" aria-label="About Us" class="mp-footer__link">About Us</a>
+                        </li>
+                        <li class="mp-footer__link-item">
+                            <a href=""
+                                aria-label="Privacy Policy" class="mp-footer__link">Privacy Policy and Confidentiality
+                                of Information</a>
+                        </li>
+                        <li class="mp-footer__link-item">
+                            <a href=""
+                                aria-label="Exchange Policy" class="mp-footer__link">Exchange, Refund and Cancellation
+                                policy</a>
+                        </li>
+                        <li class="mp-footer__link-item">
+                            <a href=""
+                                aria-label="Usage Agreement" class="mp-footer__link">Usage agreement</a>
+                        </li>
+                        <li class="mp-footer__link-item">
+                            <a href=""
+                                aria-label="Perfume Info" class="mp-footer__link">What are inspired perfumes and niche
+                                perfumes?</a>
+                        </li>
+                    </ul>
+                </div>
 
+                <!-- Third Column: Contact Information -->
+                <div class="mp-footer__column mp-footer__column--right">
+                    <h3 class="mp-footer__title">Contact us</h3>
+                    <div class="mp-footer__contacts">
+                        <a href="https://uae.matchperfumes.com/en/whatsapp/send" class="mp-contact-link"
+                            aria-label="WhatsApp">
+                            <i class="fa-brands fa-whatsapp mp-contact-icon"></i>
+                            <span class="mp-contact-text">+966125785690</span>
+                        </a>
+                        <a href="tel:920032057" class="mp-contact-link" aria-label="Phone">
+                            <i class="fa-solid fa-phone mp-contact-icon"></i>
+                            <span class="mp-contact-text">920032057</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-        </div>
-
-        <div class="mb-2.5 col-span-2">
-          <div class="">
-            <h3 class="footer-title ">Important Links</h3>
-            <ul class="footer-list store-links-items">
-              <li><a href="/page/about-us"
-                  aria-label="itme" class="text-xs lg:text-base ">About Us</a></li>
-              <li><a
-                  href="https://uae.matchperfumes.com/en/%D8%B3%D9%8A%D8%A7%D8%B3%D8%A9-%D8%A7%D9%84%D8%AE%D8%B5%D9%88%D8%B5%D9%8A%D8%A9-%D9%88%D8%B3%D8%B1%D9%8A%D8%A9-%D8%A7%D9%84%D9%85%D8%B9%D9%84%D9%88%D9%85%D8%A7%D8%AA/page-1230610191"
-                  aria-label="itme" class="text-xs lg:text-base ">Privacy Policy and Confidentiality of
-                  Information</a></li>
-              <li><a
-                  href="https://uae.matchperfumes.com/en/%D8%B3%D9%8A%D8%A7%D8%B3%D8%A9-%D8%A7%D9%84%D8%A7%D8%B3%D8%AA%D8%A8%D8%AF%D8%A7%D9%84-%D8%A3%D9%88-%D8%A7%D9%84%D8%A7%D8%B3%D8%AA%D8%B1%D8%AC%D8%A7%D8%B9/page-1963187465"
-                  aria-label="itme" class="text-xs lg:text-base ">Exchange, Refund and Cancellation policy</a></li>
-              <li><a
-                  href="https://uae.matchperfumes.com/en/%D8%A5%D8%AA%D9%81%D8%A7%D9%82%D9%8A%D8%A9-%D8%A7%D9%84%D8%A7%D8%B3%D8%AA%D8%AE%D8%AF%D8%A7%D9%85/page-280304395"
-                  aria-label="itme" class="text-xs lg:text-base ">Usage agreement</a></li>
-              <li><a
-                  href="https://uae.matchperfumes.com/en/%D9%85%D8%A7%D9%87%D9%8A-%D8%A7%D9%84%D8%B9%D8%B7%D9%88%D8%B1-%D8%A7%D9%84%D9%85%D8%B3%D8%AA%D9%88%D8%AD%D8%A7%D8%A9/page-1653750292"
-                  aria-label="itme" class="text-xs lg:text-base ">What are inspired perfumes and niche perfumes?</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-       
-        <div class="mb-2.5 col-span-2">
-    <div class="user-contacts lg:col-span-1 rounded_contacts">
-        <h3 class="footer-title">Contact us</h3>
-        <a href="https://uae.matchperfumes.com/en/whatsapp/send" class="links-contact" aria-label="contact" style="padding-top: 6px; display: block;">
-            <i class="fa-brands fa-whatsapp text-white-500 text-xl" style="padding-right: 15px;"></i>
-            <span class="text-unicode unicode break-all">+966125785690</span>
-        </a>
-        <a href="tel:920032057" class="links-contact" aria-label="contact" style="padding-top: 6px; display: block;">
-            <i class="fa-solid fa-phone text-white-600 text-xl" style="padding-right: 15px;"></i>
-            <span class="text-unicode unicode break-all">920032057</span>
-        </a>
     </div>
+
+    <!-- Footer Bottom Section -->
+    <div class="mp-footer__bottom">
+        <div class="mp-container">
+            <div class="mp-footer__bottom-content">
+                <div class="mp-footer__copyright">
+                    <p>Developed by | 2025
+                        <a href="https://introps.com/" class="mp-footer__copyright-link" target="_blank"
+                            rel="noreferrer">Introps IT</a>
+                    </p>
+                </div>
+                <div class="mp-footer__social">
+                    <ul class="mp-social-list">
+                        <li class="mp-social-item">
+                            <a href="https://www.instagram.com/matchperfumes.sa" target="_blank" title="Instagram"
+                                aria-label="Instagram" class="mp-social-link">
+                                <i class="fab fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li class="mp-social-item">
+                            <a href="https://x.com/matchperfumesa" target="_blank" title="X / Twitter"
+                                aria-label="X / Twitter" class="mp-social-link">
+                                <i class="fab fa-x-twitter"></i>
+                            </a>
+                        </li>
+                        <li class="mp-social-item">
+                            <a href="https://www.snapchat.com/add/matchperfumes" target="_blank" title="Snapchat"
+                                aria-label="Snapchat" class="mp-social-link">
+                                <i class="fab fa-snapchat"></i>
+                            </a>
+                        </li>
+                        <li class="mp-social-item">
+                            <a href="https://www.tiktok.com/@matchperfume.com" target="_blank" title="TikTok"
+                                aria-label="TikTok" class="mp-social-link">
+                                <i class="fab fa-tiktok"></i>
+                            </a>
+                        </li>
+                        <li class="mp-social-item">
+                            <a href="https://www.youtube.com/@MatchPerfumes" target="_blank" title="YouTube"
+                                aria-label="YouTube" class="mp-social-link">
+                                <i class="fab fa-youtube"></i>
+                            </a>
+                        </li>
+                        <li class="mp-social-item">
+                            <a href="https://www.facebook.com/profile.php?id=61552675531226" target="_blank"
+                                title="Facebook" aria-label="Facebook" class="mp-social-link">
+                                <i class="fab fa-facebook-f"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        
-      </div>
     </div>
-  </div>
-
-  <div class="footer-bottom">
-    <div class="container py-4">
-      <div
-        class="flex items-center justify-center lg:justify-between flex-wrap lg:flex-nowrap lg:flex-row flex-col-reverse">
-        <div class="text-sm py-2.5 footer-rights">
-          <p class="text-gray-400 mb-2.5 md:mb-0">Copyright | 2025
-            <a href="https://uae.matchperfumes.com/en" class="hover:text-primary" target="_blank"
-              rel="noreferrer">Perfumes</a>
-          </p>
-        </div>
-
-        <div class="py-2.5 lg:py-0">
-          <ul class="flex -mx-1 rounded_contacts">
-            <li class="mx-1">
-              <a href="https://www.instagram.com/matchperfumes.sa" target="_blank" title="Instagram"
-                aria-label="Instagram" class="social-link">
-                <i class="fab fa-instagram"></i>
-              </a>
-            </li>
-            <li class="mx-1">
-              <a href="https://x.com/matchperfumesa" target="_blank" title="X / Twitter" aria-label="X / Twitter"
-                class="social-link">
-                <i class="fab fa-x-twitter"></i>
-              </a>
-            </li>
-            <li class="mx-1">
-              <a href="https://www.snapchat.com/add/matchperfumes" target="_blank" title="Snapchat"
-                aria-label="Snapchat" class="social-link">
-                <i class="fab fa-snapchat"></i>
-              </a>
-            </li>
-            <li class="mx-1">
-              <a href="https://www.tiktok.com/@matchperfume.com" target="_blank" title="TikTok" aria-label="TikTok"
-                class="social-link">
-                <i class="fab fa-tiktok"></i>
-              </a>
-            </li>
-            <li class="mx-1">
-              <a href="https://www.youtube.com/@MatchPerfumes" target="_blank" title="YouTube" aria-label="YouTube"
-                class="social-link">
-                <i class="fab fa-youtube"></i>
-              </a>
-            </li>
-            <li class="mx-1">
-              <a href="https://www.facebook.com/profile.php?id=61552675531226" target="_blank" title="Facebook"
-                aria-label="Facebook" class="social-link">
-                <i class="fab fa-facebook-f"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-
-      </div>
-    </div>
-  </div>
-
-  <span
-    class="hidden w-1/2 mt-4 md:mt-12 sm:w-auto bottom-2 rtl:pl-24 ltr:pr-24 text-storeBG pointer-events-none bg-storeBG from-storeBG h-16 w-16 aspect-[3/4] lg:!w-[60%] lg:!w-[40%] object-fill object-bottom object-left object-right object-none object-top opacity-40"></span>
 </footer>
 
 {!! view_render_event('bagisto.shop.layout.footer.after') !!}
-
-<!--apply footer background-color-->
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-
-    const footer = document.querySelector('footer.store-footer.basic-footer');
-    if (footer) {
-        footer.style.backgroundColor = '#f2f2f2';
-        footer.style.color = 'black';
-        
-        const allChildren = footer.querySelectorAll('*');
-        allChildren.forEach(child => {
-            child.style.backgroundColor = '#f2f2f2';
-            child.style.color = 'black';
-        });
-    }
-});
-</script>
